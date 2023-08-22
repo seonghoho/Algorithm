d226ba9b6fa9c0cc2617d4f337566b3d1504a694
CHOI SEONGHO <128336950+seonghoho@users.noreply.github.com>
2023-08-23 01:41:17 +0900

    [Bronze II] Title: 줄 세우기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2605. 줄 세우기/줄 세우기.py
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.py
@@ -1,20 +1,7 @@
 N = int(input())
 lst = list(map(int, input().split()))
-
-result = []
-
+ans = []
 for i in range(N):
-    result.insert(i-lst[i], i+1)
-
-for j in result:
-    print(j, end=' ')
-
-# 다른 풀이
-# N = int(input())
-# temp = [0] * N
-# sequence = list(map(int, input().split()))
-# for i in range(1, N + 1):       
-#     temp[i - sequence[i - 1]:] = temp[i - sequence[i - 1] - 1:-1]
-#     temp[i - sequence[i - 1] - 1] = i 
-# 
-# print(*temp)  
+    ans.insert(i-lst[i],i+1)
+for i in ans:
+    print(i, end=' ')
