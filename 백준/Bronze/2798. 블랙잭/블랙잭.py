c446d50fc656fdc829aebbb4c76f5dbf00130ee8
CHOI SEONGHO <128336950+seonghoho@users.noreply.github.com>
2023-08-26 17:51:57 +0900

    [Bronze II] Title: 블랙잭, Time: 88 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,10 +1,14 @@
 N, M = map(int, input().split())
 lst = list(map(int, input().split()))
-num = []
-for i in range(len(lst)):
-    for j in range(i+1,len(lst)):
-        for k in range(j+1,len(lst)):
-            if lst[i]+lst[j]+lst[k] <= M:
-                num.append(lst[i]+lst[j]+lst[k])
 
-print(max(num))
+res = 0
+for i in range(N-2):
+    for j in range(i+1,N-1):
+        for k in range(j+1,N):
+            num = lst[i]+lst[j]+lst[k]
+            if num > M:
+                continue
+            else:
+                if res < num:
+                    res = num
+print(res)
