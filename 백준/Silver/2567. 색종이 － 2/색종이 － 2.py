5e69142a2eb0d840a74e9ffe28bebc01b176317d
CHOI SEONGHO <128336950+seonghoho@users.noreply.github.com>
2023-08-26 22:46:30 +0900

    [Silver IV] Title: 색종이 - 2, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
@@ -1,28 +1,21 @@
-lst = [[0]*102 for _ in range(102)]
+dohwa = [[0]*102 for _ in range(102)]
 N = int(input())
 for _ in range(N):
     x, y = map(int, input().split())
+    for j in range(x+1, x+11):
+        for i in range(y+1,y+11):
+            dohwa[i][j] = 1
 
-    for i in range(y+1,y+11):
-        for j in range(x+1,x+11):
-            lst[i][j] = 1
-
-result = 0
-for i in range(102):
-    for j in range(102):
-        if lst[i][j] == 0: continue
-        dry = [0,0,-1,1]
-        drx = [-1,1,0,0]
-        cnt = 0
-        for k in range(4):
-            dy = i + dry[k]
-            dx = j + drx[k]
-            if dy<0 or dx<0 or dy>101 or dx>101: continue
-            if lst[dy][dx] == 0:
-                cnt += 1
-        if cnt == 2:
-            result += 2
-        if cnt == 1:
-            result += 1
-
-print(result)
+dry = [0,0,-1,1]
+drx = [-1,1,0,0]
+res = 0
+for i in range(1,102):
+    for j in range(1,102):
+        if dohwa[i][j]==1:
+            for k in range(4):
+                dy = i+dry[k]
+                dx = j+drx[k]
+                if dy<0 or dx<0 or dy>101 or dx>101: continue
+                if dohwa[dy][dx]==0:
+                    res += 1
+print(res)
