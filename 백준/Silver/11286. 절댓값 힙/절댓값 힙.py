3ec07c65cca4dfe4d977eab2481a7822bbf4f438
CHOI SEONGHO <128336950+seonghoho@users.noreply.github.com>
2023-09-19 16:05:45 +0900

    [Silver I] Title: 절댓값 힙, Time: 164 ms, Memory: 40984 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -5,41 +5,14 @@ N = int(sys.stdin.readline())
 heap = []
 for i in range(N):
     num = int(sys.stdin.readline())
-    if num >0: # 양수는 b가 1
-        b = 1
-    else:
-        b = 0
     a = abs(num)
 
     if a > 0:
-        heapq.heappush(heap,[a,b])
+        heapq.heappush(heap,[a,num])
     else:
         if heap:
             back = heapq.heappop(heap)
-            if heap:
-                back2 = heapq.heappop(heap)
-                if back[0] == back2[0]:
-                    if back[1] == 0:
-                        print(-back[0])
-                        heapq.heappush(heap,back2)
-                    else:
-                        if back2[1] == 0:
-                            print(-back2[0])
-                            heapq.heappush(heap,back)
-                        else:
-                            print(back[0])
-                            heapq.heappush(heap,back2)
-                else:
-                    heapq.heappush(heap, back2)
-                    if back[1] == 0:
-                        print(-back[0])
-                    else:
-                        print(back[0])
-            else:
-                if back[1] == 0:
-                    print(-back[0])
-                else:
-                    print(back[0])
+            print(back[1])
         else:
             print(0)
 
