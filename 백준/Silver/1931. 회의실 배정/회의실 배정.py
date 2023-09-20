c72c105f049d70aaaf7e12e5013bcba99c56aee9
CHOI SEONGHO <128336950+seonghoho@users.noreply.github.com>
2023-09-20 11:41:23 +0900

    [Silver I] Title: 회의실 배정, Time: 4096 ms, Memory: 54084 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,16 +1,13 @@
-import sys
-N = int(sys.stdin.readline())
+N = int(input())
 lst = []
 for i in range(N):
-    a, b = map(int, sys.stdin.readline().strip().split())
+    a, b = map(int, input().split())
     lst.append([a,b])
-
 lst.sort(key=lambda x:(x[1],x[0]))
 cnt = 1
 num = lst[0][1]
-for i in range(1,N):
-    if num <= lst[i][0]:
-        cnt +=1
-        num = lst[i][1]
-
+for i in range(N-1):
+    if num <= lst[i+1][0]:
+        cnt += 1
+        num = lst[i+1][1]
 print(cnt)
