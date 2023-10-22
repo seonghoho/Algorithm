6d693cfa4358b0f23e8add546e84e2eecff00d0d
CHOI SEONGHO <128336950+seonghoho@users.noreply.github.com>
2023-10-22 11:19:43 +0900

    [level unrated] Title: a와 b 출력하기, Time: 134.48 ms, Memory: 69.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        String sa = "a = ";
+        String sb = "b = ";
+        System.out.println(sa + a);
+        System.out.println(sb + b);
+    }
+}
