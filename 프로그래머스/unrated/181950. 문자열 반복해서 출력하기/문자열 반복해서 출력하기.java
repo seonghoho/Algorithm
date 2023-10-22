f1e77b2816100b1a167945c242f34b9cfc770ac4
CHOI SEONGHO <128336950+seonghoho@users.noreply.github.com>
2023-10-22 11:33:16 +0900

    [level unrated] Title: 문자열 반복해서 출력하기, Time: 145.65 ms, Memory: 66.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
+++ 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
@@ -5,8 +5,11 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         int n = sc.nextInt();
-        for (int i=0; i<n;i++) {
-        System.out.print(str);
-        }
+        System.out.println(str.repeat(n));
+        // for문 사용
+        // for (int i=0; i<n;i++) {
+        // System.out.print(str);
+        // }
+        
     }
 }
