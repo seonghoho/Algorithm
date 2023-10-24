df452b06c2b49036a5d23eec8b0ae4424d491f86
CHOI SEONGHO <128336950+seonghoho@users.noreply.github.com>
2023-10-24 21:37:10 +0900

    [Silver II] Title: N과 M (10), Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,21 @@
+n, m = map(int, input().split())
+lst = sorted(list(map(int, input().split())))
+visited = [0] * n
+llst = []
+
+def dfs(level):
+    if len(llst) == m:
+        print(*llst)
+        return
+    nums = 0
+    for i in range(level,n):
+        if visited[i] == 1: continue
+        if nums != lst[i]:
+            visited[i] = 1
+            llst.append(lst[i])
+            nums = lst[i]
+            dfs(i)
+            visited[i] = 0
+            llst.pop()
+
+dfs(0)
