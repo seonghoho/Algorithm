6264fee9292d1d567d985d2e20e3ef4739a94472
CHOI SEONGHO <128336950+seonghoho@users.noreply.github.com>
2023-11-02 16:25:28 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 164 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,12 +1,12 @@
-n = int(input())
-arr = list(map(int, input().split()))
+N = int(input())
+lst = list(map(int, input().split()))
+num = [1] * N
 
-res = [1]*n # 기본적으로 1의 값을 할당
-
-for i in range(n): # n 순회하는데, i번째 값이 j번째 값보다 크면 갱신
-    num1 = arr[i]
+for i in range(N):
+    num1 = lst[i]
     for j in range(i):
-        num2 = arr[j]
-        if num1> num2:
-            res[i] = max(res[j]+1,res[i])
-print(max(res))
+        num2 = lst[j]
+        if num1>num2:
+            num[i] = max(num[i], num[j]+1)
+
+print(max(num))
