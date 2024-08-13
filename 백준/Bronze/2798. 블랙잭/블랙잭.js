a4e036d72021b122596d6145bb45499b8e0dc913
Choi Seongho <128336950+seonghoho@users.noreply.github.com>
2024-08-13 22:53:44 +0900

    [Bronze II] Title: 블랙잭, Time: 172 ms, Memory: 9604 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.js
+++ 백준/Bronze/2798. 블랙잭/블랙잭.js
@@ -4,19 +4,15 @@ const input = require("fs")
   .trim()
   .split("\n");
 
-const NM = input[0].split(" ");
-// 카드 갯수
-const N = NM[0];
-// 딜러가 부르는 수
-const M = NM[1];
-// 카드 목록을 오름차순으로
-const lst = input[1]
+const [N, M] = input.shift().split(" ");
+
+let lst = input[0]
   .split(" ")
   .map(Number)
   .sort((a, b) => a - b);
 
-let max = 0;
 let sum = 0;
+let max = 0;
 
 for (let i = 0; i < lst.length - 2; i++) {
   sum = 0;
@@ -25,17 +21,7 @@ for (let i = 0; i < lst.length - 2; i++) {
     sum = lst[i];
     sum += lst[j];
     for (let k = j + 1; k < lst.length; k++) {
-      //   console.log("i = " + i);
-      //   console.log("j = " + j);
-      //   console.log("k = " + k);
-      //   console.log("sum = " + sum);
-      //   console.log("lst[k] = " + lst[k]);
-
-      if (sum + lst[k] > max && sum + lst[k] <= M) {
-        max = sum + lst[k];
-        // console.log("max = " + max);
-        // console.log("-------------");
-      }
+      if (max < sum + lst[k] && sum + lst[k] <= M) max = sum + lst[k];
     }
   }
 }
