c6344feb4264c7a2274dbd7526716984f604a532
Choi Seongho <128336950+seonghoho@users.noreply.github.com>
2024-09-12 23:28:04 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,6 @@
+function solution(phone_number) {
+    
+    let blindNums = new Array(phone_number.length-4).fill('*').join('')
+    let lastNums = phone_number.slice(phone_number.length-4, phone_number.length)
+    return blindNums + lastNums
+}
