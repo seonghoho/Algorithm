b4901e45147eefb350cc823c1f7c2406f1724f35
Choi Seongho <128336950+seonghoho@users.noreply.github.com>
2024-09-13 13:09:30 +0900

    [level 1] Title: 하샤드 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,5 +1,5 @@
 function solution(x) {
     let number = x.toString().split('').map(Number).reduce((res,current) => res + current,0)
 
-    return x % number ? false : true
+    return !(x % number)
 }
