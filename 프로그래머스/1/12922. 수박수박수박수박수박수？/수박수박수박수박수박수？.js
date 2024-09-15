cbeddc14ee2062e31e575f5cb878b5541039d015
Choi Seongho <128336950+seonghoho@users.noreply.github.com>
2024-09-15 21:52:48 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,7 +1,9 @@
 function solution(n) {
-    let str = ''
-    for(let i=0; i< Math.floor(n/2); i++) {
-            str += '수박'
-        }
-    return n%2 ? str+'수' : str
+    // let str = ''
+    // for(let i=0; i< Math.floor(n/2); i++) {
+    //         str += '수박'
+    //     }
+    // return n%2 ? str+'수' : str
+    
+    return ('수박').repeat(n).slice(0,n)
 }
