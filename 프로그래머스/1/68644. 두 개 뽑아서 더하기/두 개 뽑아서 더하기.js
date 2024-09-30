7f6a2d47e458df71d7f8b2e3e52084136deee0e7
Choi Seongho <128336950+seonghoho@users.noreply.github.com>
2024-09-30 12:38:37 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.46 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+    let newSet = new Set()
+    for(let i=0; i< numbers.length-1; i++) {
+        for(let j=i+1; j< numbers.length; j++) {
+            newSet.add(numbers[i]+ numbers[j])
+        }
+    }
+    let resArr = new Array()
+    newSet.forEach((num) => {
+        resArr.push(num)
+    })
+    return resArr.sort((a,b) => a-b)
+}
