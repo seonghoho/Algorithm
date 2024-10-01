972c09efc7a860306a62fa18a523683ffc92aa1d
Choi Seongho <128336950+seonghoho@users.noreply.github.com>
2024-10-01 13:04:00 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 43.67 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
 
 ### 성능 요약
 
-메모리: 83.5 MB, 시간: 35.53 ms
+메모리: 83 MB, 시간: 43.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 01일 12:45:28
+2024년 10월 01일 13:03:57
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,7 +1,8 @@
 function solution(arr) {
-   let res = [arr[0]]
-   for(let i=1; i<arr.length; i++) {
-       arr[i] === res[res.length-1] ? '' : res.push(arr[i])
-   }
-    return res
+//   let res = [arr[0]]
+//   for(let i=1; i<arr.length; i++) {
+//       arr[i] === res[res.length-1] ? '' : res.push(arr[i])
+//   }
+//    return res
+    return arr.filter((num,index) => num !== arr[index+1])
 }
