cedd5840965b5c55c93218aaf43042caeefb924d
Choi Seongho <128336950+seonghoho@users.noreply.github.com>
2024-11-24 21:37:58 +0900

    [level 1] Title: 햄버거 만들기, Time: 262.03 ms, Memory: 97.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,18 @@
+function solution(ingredient) {
+    let stack = [];
+    let count = 0;
+
+    for (let i = 0; i < ingredient.length; i++) {
+        stack.push(ingredient[i]);
+
+        if (stack.length >= 4) {
+            let lastFour = stack.slice(-4);
+            if (lastFour.join('') === '1231') {
+                stack.splice(-4);
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
