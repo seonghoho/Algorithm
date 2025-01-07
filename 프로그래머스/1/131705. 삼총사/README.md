81b5ef7dd7a43e60910a27cd672103aa45d57ea6
Choi Seongho <128336950+seonghoho@users.noreply.github.com>
2025-01-07 21:01:08 +0900

    [level 1] Title: 삼총사, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/README.md
+++ 프로그래머스/1/131705. 삼총사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 삼총사 - 131705 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131705) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131705?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.14 ms
+메모리: 33.4 MB, 시간: 0.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 25일 23:42:46
+2025년 01월 07일 21:01:05
 
 ### 문제 설명
 
--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,9 +1,9 @@
 function solution(number) {
-    let cnt = 0
-    for(let i=0; i<number.length-2; i++) {
-        for(let j=i+1; j<number.length-1; j++) {
+    cnt = 0
+    for(let i=0; i<number.length; i++) {
+        for(let j=i+1; j<number.length; j++) {
             for(let k=j+1; k<number.length; k++) {
-                number[i] + number[j] + number[k]  ? '' : cnt++
+                if(number[i] + number[j] + number[k]===0) cnt +=1
             }
         }
     }
