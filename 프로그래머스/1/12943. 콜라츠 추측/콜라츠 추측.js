1acf14ae7b490df5f4a542409985194a786f1b08
Choi Seongho <128336950+seonghoho@users.noreply.github.com>
2025-01-15 20:46:26 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,24 +1,9 @@
 function solution(num) {
-     if (num === 1) {
-         return 0
-     } else {
-    let res = num;
-    let cnt = 0;
-    
-    while (res !== 1) {
-        if (cnt > 500) {
-            cnt= -1; 
-            break
-        } else {
-        if (res % 2 === 0) {
-            res /= 2;
-            cnt += 1;
-        } else {
-            res *= 3;
-            res += 1;
-            cnt += 1;           
-        }}
+    let n = num
+    let cnt = 0
+    while (n !== 1 && cnt !== 500) {
+        n % 2 == 0 ? n = n / 2 : n = n * 3 + 1
+        cnt += 1
     }
-    return cnt
-     }
+    return n == 1 ? cnt < 500 ? cnt : -1 : -1
 }
